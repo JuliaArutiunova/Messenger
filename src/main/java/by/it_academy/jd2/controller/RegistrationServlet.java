eb776e6a00f2cf698b36ecdeacec7ebcfadbfa76
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-10-24 18:38:41 +0300

    RegistrationServlet: добавлена аннотация @WebServlet,

--- src/main/java/by/it_academy/jd2/controller/RegistrationServlet.java
+++ src/main/java/by/it_academy/jd2/controller/RegistrationServlet.java
@@ -6,6 +6,7 @@ import by.it_academy.jd2.exception.RegistrationException;
 import by.it_academy.jd2.service.api.IUserService;
 import by.it_academy.jd2.service.factory.ServiceFactory;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,6 +15,7 @@ import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 
+@WebServlet(name = "RegistrationServlet", urlPatterns = "/api/user")
 public class RegistrationServlet extends HttpServlet {
 
     private static final String REGISTRATION_JSP_PATH = "/template/Registration.jsp";
