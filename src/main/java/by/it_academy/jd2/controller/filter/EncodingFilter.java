bc4bc0d457e484e117cb67548babe3214bd049bd
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-10-24 18:21:04 +0300

    добавлена аннотация @WebFilter для EncodingFilter

--- src/main/java/by/it_academy/jd2/controller/filter/EncodingFilter.java
+++ src/main/java/by/it_academy/jd2/controller/filter/EncodingFilter.java
@@ -1,10 +1,11 @@
 package by.it_academy.jd2.controller.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 
 import java.io.IOException;
 
-
+@WebFilter(filterName = "EncodingFilter", urlPatterns = {"/*"})
 public class EncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
