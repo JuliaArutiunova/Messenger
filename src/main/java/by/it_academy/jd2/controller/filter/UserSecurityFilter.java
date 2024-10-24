62e5c6fe49038fc255859212112c2269062d51cc
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-10-24 18:29:58 +0300

    UserSecurityFilter: добавлена аннотация @WebFilter, исправлен путь в sendRedirect

--- src/main/java/by/it_academy/jd2/controller/filter/UserSecurityFilter.java
+++ src/main/java/by/it_academy/jd2/controller/filter/UserSecurityFilter.java
@@ -1,12 +1,13 @@
 package by.it_academy.jd2.controller.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-
+@WebFilter(filterName = "UserSecurityFilter", urlPatterns = {"/ui/user/*", "/api/message"})
 public class UserSecurityFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
@@ -20,7 +21,7 @@ public class UserSecurityFilter implements Filter {
         if ((session != null) && (session.getAttribute("user") != null)) {
             chain.doFilter(request, response);
         } else {
-            resp.sendRedirect(contextPath + "/api/login");
+            resp.sendRedirect(contextPath + "/ui/login");
         }
 
     }
