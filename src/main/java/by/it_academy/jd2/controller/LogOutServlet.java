af48f50e8ed1e0940769d2ec47d3c11430f5581e
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-10-24 18:40:56 +0300

    ExitServlet переименован в LogOutServlet, добавлена аннотация @WebServlet, исправлен путь в sendRedirect

--- src/main/java/by/it_academy/jd2/controller/LogOutServlet.java
+++ src/main/java/by/it_academy/jd2/controller/LogOutServlet.java
@@ -1,19 +1,20 @@
 package by.it_academy.jd2.controller;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
 import java.io.IOException;
-
-public class ExitServlet extends HttpServlet {
+@WebServlet(name = "LogOutServlet", urlPatterns = "/api/logout")
+public class LogOutServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         req.getSession().removeAttribute("user");
 
-        resp.sendRedirect(req.getContextPath() + "/api/login");
+        resp.sendRedirect(req.getContextPath());
     }
 }
