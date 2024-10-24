2891e7920f6061e9e6889f398787e69c122a8cfd
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-10-24 18:23:43 +0300

    добавлена аннотация @WebFilter для AdminSecurityFilter

--- src/main/java/by/it_academy/jd2/controller/filter/AdminSecurityFilter.java
+++ src/main/java/by/it_academy/jd2/controller/filter/AdminSecurityFilter.java
@@ -3,6 +3,7 @@ package by.it_academy.jd2.controller.filter;
 import by.it_academy.jd2.dto.UserDTO;
 import by.it_academy.jd2.entity.UserRole;
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -11,6 +12,7 @@ import java.io.IOException;
 
 import static by.it_academy.jd2.controller.StatisticServlet.STATISTIC_JSP;
 
+@WebFilter(filterName = "AdminSecurityFilter", urlPatterns = {"/ui/admin/*", "/api/admin/*"})
 public class AdminSecurityFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -25,7 +27,7 @@ public class AdminSecurityFilter implements Filter {
             if (user.getRole().equals(UserRole.ADMIN)) {
                 chain.doFilter(request, response);
             } else {
-                resp.sendRedirect(contextPath + "/api/login");
+                resp.sendRedirect(contextPath + "/ui/login");
             }
         }
 
