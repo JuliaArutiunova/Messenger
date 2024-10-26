536647ea4ef0c3877fae23ae39976d93319a65cb
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-10-26 13:31:51 +0300

    Добавлена проверка в LogOutServlet

--- src/main/java/by/it_academy/jd2/controller/LogOutServlet.java
+++ src/main/java/by/it_academy/jd2/controller/LogOutServlet.java
@@ -5,15 +5,21 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 
 import java.io.IOException;
+
 @WebServlet(name = "LogOutServlet", urlPatterns = "/api/logout")
 public class LogOutServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        req.getSession().removeAttribute("user");
+        HttpSession session = req.getSession();
+
+        if (session.getAttribute("user") != null) {
+            session.removeAttribute("user");
+        }
 
         resp.sendRedirect(req.getContextPath());
     }
