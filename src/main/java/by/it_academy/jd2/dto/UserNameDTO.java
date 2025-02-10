082a8de836d21ac6aba10d42d53181d5ca2bd8e0
Julia Arutiunova <j.arutiunova1@gmail.com>
2025-02-10 15:30:14 +0300

    Создан UserNameDTO

---
+++ src/main/java/by/it_academy/jd2/dto/UserNameDTO.java
@@ -0,0 +1,15 @@
+package by.it_academy.jd2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserNameDTO {
+    private String login;
+    private String name;
+
+
+}
